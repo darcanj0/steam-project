9fdc0aad0494acfbcdd804aeaad6554deb6e295a
Daniel Arcanjo <daniel.thomas.aarcanjo@gmail.com>
2022-06-26 02:29:14 -0300

    chore(user): modify user schema, doc user dto

--- src/User/dto/create-user.dto.ts
+++ src/User/dto/create-user.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsBoolean,
   IsEmail,
@@ -9,22 +10,44 @@ import {
 export class CreateUserDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    description: 'Name of the user',
+    example: 'Daniel',
+  })
   userName: string;
 
   @IsEmail()
+  @ApiProperty({
+    description: 'User email',
+    example: 'name@example.com',
+  })
   email: string;
 
   @IsString()
   @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/)
   @IsNotEmpty()
+  @ApiProperty({
+    description: `at least 1 lowercase alphabetical\nat least 1 uppercase alphabetical\nat least 1 numeric\none special character`,
+    example: 'Abcdef@1',
+  })
   password: string;
 
   @IsBoolean()
   @IsNotEmpty()
+  @ApiProperty({
+    description: 'True if the user is an admin; False if not',
+    example: true,
+  })
   isAdmin: boolean;
 
+  //ex: 12345678910
   @IsString()
   @Matches(/(\d{3})(\d{3})(\d{3})(\d{2})/)
   @IsNotEmpty()
+  @ApiProperty({
+    description:
+      "User's CPF as a string of numbers. (Brazilian equivalent of the SSN)",
+    example: '12345678900',
+  })
   cpf: string;
 }
