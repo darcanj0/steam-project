a1f536911c691eeb3265a747370221c6dc6e1f6f
Daniel Arcanjo <daniel.thomas.aarcanjo@gmail.com>
2022-06-26 00:15:18 -0300

    docs: swagger setup

--- src/User/user.controller.ts
+++ src/User/user.controller.ts
@@ -1,6 +1,8 @@
 import { Controller } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { UserService } from './user.service';
 
+@ApiTags('user')
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle(`Steam-project`)
+    .setDescription(`User-Profile-Games management API`)
+    .setVersion('1.0.0')
+    .addTag('status')
+    .addTag('user')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3001);
 }
