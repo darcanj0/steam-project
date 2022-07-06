5b3dde4d313fe078212658ca7f2d81f26f2ca4a1
Daniel Arcanjo <daniel.thomas.aarcanjo@gmail.com>
2022-07-05 22:12:41 -0300

    refactor(genre): refactor creation dto

--- src/Genre/dto/create-genre.dto.ts
+++ src/Genre/dto/create-genre.dto.ts
@@ -3,8 +3,8 @@ import { IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CreateGenreDto {
   @IsString()
-  @MinLength(3)
-  @MaxLength(50)
+  @MinLength(3, {message: 'Title is too short'})
+  @MaxLength(30, {message: 'Title is too long'})
   @ApiProperty({
     description: 'Name of the genre',
     example: 'Survival Horror',
