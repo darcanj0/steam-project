0a3a94f0ab5913905eb22270bc31878a37544108
Daniel Arcanjo <daniel.thomas.aarcanjo@gmail.com>
2022-07-05 21:53:07 -0300

    refactor(genre): update creation dto constraints

--- src/Genre/dto/create-genre.dto.ts
+++ src/Genre/dto/create-genre.dto.ts
@@ -1,9 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CreateGenreDto {
   @IsString()
-  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(50)
   @ApiProperty({
     description: 'Name of the genre',
     example: 'Survival Horror',
