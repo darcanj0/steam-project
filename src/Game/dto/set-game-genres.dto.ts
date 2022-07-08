bf1811adf558210310c719227765a075cf431bf1
Daniel Arcanjo <daniel.thomas.aarcanjo@gmail.com>
2022-07-08 13:20:06 -0300

    udpate dto set genres

--- src/Game/dto/set-game-genres.dto.ts
+++ src/Game/dto/set-game-genres.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsInt, IsPositive, IsUUID } from 'class-validator';
+import { IsInt, IsPositive } from 'class-validator';
 
 export class SetGameGenresDto {
   @IsInt({ each: true })
