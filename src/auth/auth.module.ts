58037bfaf60918f67b3a85c8f16147737746943d
Daniel Arcanjo <daniel.thomas.aarcanjo@gmail.com>
2022-07-15 21:38:12 -0300

    24h token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -12,7 +12,7 @@ import { JwtStrategy } from './jwt.strategies';
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '8h' },
+      signOptions: { expiresIn: '24h' },
     }),
   ],
   controllers: [AuthController],
